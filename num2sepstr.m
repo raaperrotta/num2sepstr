4052b1ae4e301870bf7a98628ea8735cc91c9b2a
Robert Perrotta <raaperrotta@gmail.com>
2016-08-30 19:48:13 -0400

    no change; testing git hooks

--- num2sepstr.m
+++ num2sepstr.m
@@ -1,18 +1,18 @@
 function stringout = num2sepstr(numin,format)
 % NUM2SEPSTR Convert to string with comma separation at thousands.
-% 
+%
 % stringout = NUM2SEPSTR(numin,[format]) formats numin to a string
 %   according to the specified format and adds commas as thousands
 %   seperators. See <a href="matlab:help num2str">num2str</a> for behavior
 %   when format is omitted.
-% 
+%
 %   String length is ignored; Instead of...
 %   >> sprintf('%s\n',num2sepstr(1e6,'% 20.2f'))
 %   ...try...
 %   >> sprintf('% 20s\n',num2sepstr(1e6,'%.2f'))
-% 
+%
 % See also SPRINTF, NUM2STR
-% 
+%
 % Created by:
 %   Robert Perrotta
 
